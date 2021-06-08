fe7b9cc4c2638a9a457c535c40037bb187ce2d6e
Dmitrii Tuchs <d.tuchs@propellerads.net>
2021-06-08 21:36:51 +0300

    Added message to main() method

--- src/main/java/guru/qa/Application.java
+++ src/main/java/guru/qa/Application.java
@@ -3,6 +3,7 @@ package guru.qa;
 public class Application {
 
     public static void main(String[] args) {
+        System.out.println("This is the main method");
         new Calculator().start();
     }
 }
