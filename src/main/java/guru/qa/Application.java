50037652e8dc33cfc43d23d417defa8924a14565
Dmitrii Tuchs <d.tuchs@propellerads.net>
2021-06-08 21:40:21 +0300

    Just for rebase

--- src/main/java/guru/qa/Application.java
+++ src/main/java/guru/qa/Application.java
@@ -5,5 +5,6 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("This is the main method");
         new Calculator().start();
+         System.out.println("Just for rebase demo");
     }
 }
