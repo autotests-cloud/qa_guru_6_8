d6fda062ebab0d3f396ffff6559e6f141c615e49
Dmitrii Tuchs <d.tuchs@propellerads.net>
2021-06-08 21:28:57 +0300

    Update Calculator.java
    added :(

--- src/main/java/guru/qa/Calculator.java
+++ src/main/java/guru/qa/Calculator.java
@@ -4,7 +4,7 @@ public class Calculator {
 
     public void start() {
         System.out.println("Nata");
-        System.out.println("2 line");
+        System.out.println("2 line :(");
         System.out.println("calculator stopped!");
     }
 }
