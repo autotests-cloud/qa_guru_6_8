4350dbb37c21761c135724840e63ccb4f1930079
Natalic2020 <65713606+Natalic2020@users.noreply.github.com>
2021-06-08 20:22:36 +0200

    commit (#3)
    Co-authored-by: Dmitrii Tuchs

--- src/main/java/guru/qa/Calculator.java
+++ src/main/java/guru/qa/Calculator.java
@@ -3,7 +3,7 @@ package guru.qa;
 public class Calculator {
 
     public void start() {
-        System.out.println("calculator started!");
+        System.out.println("Nata");
         System.out.println("2 line");
         System.out.println("calculator stopped!");
     }
