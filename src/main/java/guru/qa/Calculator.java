a4838ac2cb6acd3d80a8809c6be2a7f7de4d43ac
natal <svetnata74@mail.ru>
2021-06-08 20:12:39 +0200

    commit

--- src/main/java/guru/qa/Calculator.java
+++ src/main/java/guru/qa/Calculator.java
@@ -1,8 +1,10 @@
 package guru.qa;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
 public class Calculator {
 
     public void start() {
-
+        System.out.println("Nata");
     }
 }
