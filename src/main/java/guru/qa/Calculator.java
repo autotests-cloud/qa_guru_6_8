09a3e0f2afa494f800758ab341c1a7ee8e13cb96
dtuchs <d.tuchs@propellerads.net>
2021-06-08 21:15:15 +0300

    3 lines commit

--- src/main/java/guru/qa/Calculator.java
+++ src/main/java/guru/qa/Calculator.java
@@ -3,6 +3,8 @@ package guru.qa;
 public class Calculator {
 
     public void start() {
-
+        System.out.println("calculator started!");
+        System.out.println("2 line");
+        System.out.println("calculator stopped!");
     }
 }
