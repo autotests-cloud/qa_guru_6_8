153c16e25d14381c86fa7a0b1e9c894748ce0be4
DaniilAutoQA <daniil-bond@yandex.ru>
2021-06-18 10:06:57 +0300

    fixed bug3

--- src/main/java/guru/qa/Application.java
+++ src/main/java/guru/qa/Application.java
@@ -3,9 +3,6 @@ package guru.qa;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("This is the main method");
-        new Calculator().start();
-         System.out.println("Just for rebase demo");
-        System.out.println("sdf");
+        System.out.println("fixed");
     }
 }
